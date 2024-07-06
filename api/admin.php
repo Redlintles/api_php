429c90a4dcac442af55576290c68a12e0224796c
redlintles <treelands54@gmail.com>
2024-07-06 16:45:51 -0300

    Adicionado Controller GET para admin

--- api/admin.php
+++ api/admin.php
@@ -1,7 +1,21 @@
 <?php
 
 
+require $_SERVER["DOCUMENT_ROOT"] . "/functions/PermissionValidator.php";
+
+
 // Verifica o método da requisição
 if ($_SERVER["REQUEST_METHOD"] === "GET") {
-    echo "Banana"; // Apenas um exemplo de resposta para método GET
+    $user = \Buildings\AdminQuery::create()->findOneByApiKey($_GET["api_key"]);
+    $permission = \Buildings\PermissionQuery::create()->findOneByAdminId($user->getId());
+
+    permissionValidator($permission, "READ");
+    if($user->getUsername() === "root") {
+        $users = \Buildings\AdminQuery::create()->find()->toJSON();
+        http_response_code(200);
+        echo $users;
+    } else {
+        http_response_code(200);
+        echo json_encode($user->toArray());
+    }
 }
