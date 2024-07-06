1aa146cf14bb6237dc2f09682d08f4a5d7785c89
redlintles <treelands54@gmail.com>
2024-07-06 16:56:44 -0300

    Modificada estrutura de roteamento do projeto

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ switch ($request) {
         echo "<h1>Hello World!</h1>";
         break;
     case "/api/admin":
-        require __DIR__ . "/api/admin.php";
+        require __DIR__ . "/api/Admin/router.php";
         break;
     default:
         // Rota padrão para tratamento de 404
