5c0ad18f4b12880ca9e7f492ce486051d3711164
redlintles <treelands54@gmail.com>
2024-07-06 11:27:39 -0300

    Inicialização de root finalizada

--- functions/initDb.php
+++ functions/initDb.php
@@ -3,6 +3,20 @@
 
 use Ramsey\Uuid\Uuid;
 
+function createRootPermission()
+{
+    $root = \Buildings\AdminQuery::create()->findOneByUsername("root");
+    if(isset($root)) {
+        $permission = new \Buildings\Permission();
+        $permission->setAdminId($root->getId());
+        $permission->setCreatePermission("1");
+        $permission->setReadPermission("1");
+        $permission->setUpdatePermission("1");
+        $permission->setDeletePermission("1");
+        $permission->save();
+    }
+}
+
 function initRoot()
 {
     $adm = \Buildings\AdminQuery::create()->findOneByUsername("root");
@@ -12,16 +26,11 @@ function initRoot()
         $root->setPassword("root123");
         $root->setApiKey(Uuid::uuid4()->toString());
         $root->save();
-        $localAdm = \Buildings\AdminQuery::create()->findOneByUsername("root");
-        $result = \Buildings\PermissionQuery::create()->findOneByAdminId($localAdm->getId());
-        if(!isset($result)) {
-            $rootPermission = new \Buildings\Permission();
-            $rootPermission->setAdminId($localAdm->getId());
-            $rootPermission->setCreate("1");
-            $rootPermission->setRead("1");
-            $rootPermission->setUpdate("1");
-            $rootPermission->setDelete("1");
-            $rootPermission->save();
+        createRootPermission();
+    } elseif(isset($adm)) {
+        $permission = \Buildings\PermissionQuery::create()->findOneByAdminId($adm->getId());
+        if(!isset($permission)) {
+            createRootPermission();
         }
     }
 }
