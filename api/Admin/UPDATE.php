2a81e92b4dea1f7a368cbbb0237bddaf233817b6
redlintles <treelands54@gmail.com>
2024-07-08 15:44:34 -0300

    Adicionada rota Admin UPDATE

---
+++ api/Admin/UPDATE.php
@@ -0,0 +1,70 @@
+<?php
+
+require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/bodyParser.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/PermissionValidator.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/SendResponse.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/DataValidation.php";
+
+$body = bodyParser();
+
+$apiKey = $_SERVER["HTTP_X_API_KEY"];
+
+permissionValidator($apiKey, "UPDATE");
+
+if(!isset($body["admin_id"])) {
+    sendResponse(400, true, "admin_id not defined");
+}
+if(isset($body["username"])) {
+    validateUsername($body["username"]);
+}
+
+if(isset($body["password"])) {
+    validatePassword($body["password"]);
+}
+
+$targetUser = \Buildings\AdminQuery::create()->findOneById($body["admin_id"]);
+$isRoot = \Buildings\AdminQuery::create()->findOneByApiKey($apiKey)->getUsername() === "root";
+
+
+if(!isset($targetUser)) {
+    sendResponse(400, true, "Admin not found");
+}
+
+if($targetUser->getUsername() === "root") {
+
+    if(!$isRoot) {
+        sendResponse(400, true, "Only root can change it's own password");
+    }
+    if(isset($body["username"])) {
+        sendResponse(400, true, "Root username cannot be changed, only password");
+    }
+    if(!isset($body["password"])) {
+        sendResponse(400, true, "Root new password not specified");
+    } else {
+        $targetUser->setPassword($body["password"]);
+        $targetUser->save();
+        sendResponse(200, false, "Root Password changed successfully");
+    }
+} elseif(!$isRoot && $targetUser->getApiKey() === $apiKey) {
+    if(isset($body["username"])) {
+        $targetUser->setUsername($body["username"]);
+    }
+
+    if(isset($body["password"])) {
+        $targetUser->setPassword($body["password"]);
+    }
+
+    $targetUser->save();
+    sendResponse(200, false, "You have changed your own username(and/or)password Successfully");
+} elseif($isRoot) {
+    if(isset($body["username"])) {
+        $targetUser->setUsername($body["username"]);
+    }
+
+    if(isset($body["password"])) {
+        $targetUser->setPassword($body["password"]);
+    }
+    $targetUser->save();
+    sendResponse(200, false, "Root has changed username(and/or)password successfully");
+
+}
--- api/Admin/router.php
+++ api/Admin/router.php
@@ -6,4 +6,6 @@ if($_SERVER["REQUEST_METHOD"] === "GET") {
     require_once __DIR__ . "/POST.php";
 } elseif($_SERVER["REQUEST_METHOD"] === "DELETE") {
     require_once __DIR__ . "/DELETE.php";
+} elseif($_SERVER["REQUEST_METHOD"] === "PUT") {
+    require_once __DIR__ . "/UPDATE.php";
 }
