b7856921503d3622bd4f29b3ac1f0b5ea4a93859
redlintles <treelands54@gmail.com>
2024-07-08 19:55:12 -0300

    Código de GET.php refatorado

--- api/Admin/GET.php
+++ api/Admin/GET.php
@@ -1,27 +1,42 @@
 <?php
 
-use Propel\Runtime\ActiveQuery\Criteria;
 
 require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/PermissionValidator.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/SendResponse.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/bodyParser.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/CollectionToArray.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/DataValidation.php";
 
 
 $apiKey = $_SERVER["HTTP_X_API_KEY"];
 
 permissionValidator($apiKey, "READ");
 
+$body = bodyParser();
+
 $user = \Buildings\AdminQuery::create()->findOneByApiKey($apiKey);
 
+
 if(isset($user) && $user->getUsername() === "root") {
-    $users = \Buildings\AdminQuery::create()->find();
 
-    $resultUsers = [];
+    $targetUser = null;
 
-    foreach($users as $admin) {
-        $obj = $admin->toArray();
-        array_push($resultUsers, $obj);
+
+    if(isset($body["admin_id"])) {
+        validateId($body["admin_id"]);
+        $targetUser = \Buildings\AdminQuery::create()->findOneById($body["admin_id"]);
+    } elseif(isset($body["username"])) {
+        validateUsername($body["username"]);
+        $targetUser = \Buildings\AdminQuery::create()->findOneByUsername($body["username"]);
     }
-    sendResponse(200, false, "Users fetched successfully", ["users" => $resultUsers]);
+    if(!isset($targetUser)) {
+        $users = \Buildings\AdminQuery::create()->find();
+
+        $resultUsers = collectionToArray($users);
+        sendResponse(200, false, "No criteria specified(admin_id,username) fetching all admins", ["users" => $resultUsers]);
+    } else {
+        sendResponse(200, false, "Data from " . $targetUser->getUsername() . " Fetched successfully", $targetUser->toArray());
+    }
 } elseif(isset($user)) {
     sendResponse(200, false, "User data fetched successfully", $user->toArray());
 } else {
