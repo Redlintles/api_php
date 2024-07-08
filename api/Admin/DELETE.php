e1720cf32e7ab7bcec31c789ee79584d5a4b5073
redlintles <treelands54@gmail.com>
2024-07-08 19:42:10 -0300

    Código de DELETE.php refatorado

--- api/Admin/DELETE.php
+++ api/Admin/DELETE.php
@@ -13,23 +13,28 @@ $body = bodyParser();
 
 $user = \Buildings\AdminQuery::create()->findOneByApiKey($apiKey);
 
-if(isset($user) && $user->getUsername() === "root") {
+if($user->getUsername() === "root") {
 
-    if($body["username"] === "root") {
+    $targetUser = null;
+
+    if(isset($body["admin_id"])) {
+        validateId($body["admin_id"]);
+        $targetUser = \Buildings\AdminQuery::create()->findOneById($body["admin_id"]);
+    } elseif(isset($body["username"])) {
+        validateUsername($body["username"]);
+        $targetUser = \Buildings\AdminQuery::create()->findOneByUsername($body["username"]);
+    }
+
+    if(!isset($targetUser)) {
+        sendResponse(400, true, "User " . $body["username"] . " not found");
+    }
+
+    if($targetUser->getUsername() === "root") {
         sendResponse(400, true, "Root cannot be deleted");
     }
 
-    $user = \Buildings\AdminQuery::create()->findOneByUsername($body["username"]);
-
-    if(!isset($user)) {
-        sendResponse(400, true, "User " . $body["username"] . " not found");
-    } else {
-        $user->delete();
-        sendResponse(200, false, "User " . $body["username"] . " Deleted successfully");
-    }
-
-
-
-} elseif(isset($user)) {
+    $targetUser->delete();
+    sendResponse(200, false, "User " . $body["username"] . " Deleted successfully");
+} else {
     sendResponse(403, true, "Only root can delete admins");
 }
