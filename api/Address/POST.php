a23d1f24c84c7169b28dc358d90e7870d0c2e806
redlintles <treelands54@gmail.com>
2024-07-08 22:27:34 -0300

    Adicionada rota Adress POST

---
+++ api/Address/POST.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/SendResponse.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/bodyParser.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/DataValidation.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/PermissionValidator.php";
+
+$body = bodyParser();
+
+$apiKey = $_SERVER["HTTP_X_API_KEY"];
+permissionValidator($apiKey, "CREATE");
+
+$addressObj = [];
+if(isset($body["country"])) {
+    $validateLocation($body["country"]);
+    $addressObj["country"] = $body["country"];
+}
+if(isset($body["state"])) {
+    $validateState($body["state"]);
+    $addressObj["state"] = $body["state"];
+}
+if(isset($body["city"])) {
+    $validateLocation($body["city"]);
+    $addressObj["city"] = $body["city"];
+}
+if(isset($body["neighborhood"])) {
+    $validateLocation($body["neighborhood"]);
+    $addressObj["neighborhood"] = $body["neighborhood"];
+}
+if(isset($body["street"])) {
+    $validateLocation($body["street"]);
+    $addressObj["street"] = $body["street"];
+}
+if(isset($body["house_number"])) {
+    $validateHouseNumber($body["house_number"]);
+    $addressObj["number"] = $body["house_number"];
+}
+
+$obj = new \Buildings\Address();
+
+$obj->setCountry($addressObj["country"]);
+$obj->setCity($addressObj["city"]);
+$obj->setState($addressObj["state"]);
+$obj->setStreet($addressObj["street"]);
+$obj->setNeighborhood($addressObj["neighborhood"]);
+$obj->setNumber($addressObj["number"]);
+$obj->save();
+
+if(isset($obj)) {
+    sendResponse(200, false, "Address Created Successfully", $obj->toArray());
+} else {
+    sendResponse(500, true, "An unexpected error ocurred");
+}
