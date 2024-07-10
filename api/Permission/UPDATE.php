6ab21d6c98d30c59988add16c47b8fe9ad7689e0
redlintles <treelands54@gmail.com>
2024-07-10 19:47:53 -0300

    Corrigidos erros causados pela mudança em Permission Update

--- api/Permission/UPDATE.php
+++ api/Permission/UPDATE.php
@@ -4,15 +4,21 @@ require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/SendResponse.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/PermissionValidator.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/bodyParser.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/DataValidation.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/Audit.php";
 
 
 $body = bodyParser();
 
+
 $apiKey = $_SERVER["HTTP_X_API_KEY"];
 $user = \Buildings\AdminQuery::create()->findOneByApiKey($apiKey);
+$auditObj = new AuditObj($apikey, "UPDATE", $request);
+$auditObj->setOperation("ChangePermission");
 
 if(!isset($user)) {
-    sendResponse(404, true, "User not found");
+    sendResponse(404, true, "User not found", [], [
+        "audit" => $auditObj
+    ]);
 }
 
 permissionValidator($apiKey, "UPDATE");
@@ -20,31 +26,43 @@ permissionValidator($apiKey, "UPDATE");
 $targetUser = null;
 
 if(isset($body["admin_id"])) {
-    validateId($body["admin_id"]);
+    $validateInteger($body["admin_id"]);
     $targetUser = \Buildings\AdminQuery::create()->findOneById($body["admin_id"]);
 } elseif(isset($body["username"])) {
-    validateUsername($body["username"]);
+    $validateUsername($body["username"]);
     $targetUser = \Buildings\AdminQuery::create()->findOneByUsername($body["username"]);
 } else {
-    sendResponse(400, true, "No criteria for target user specified, body must contain either admin_id or username keys");
+    sendResponse(400, true, "No criteria for target user specified, body must contain either admin_id or username keys", [], [
+        "audit" => $auditObj
+    ]);
 }
 
 if(!isset($targetUser)) {
-    sendResponse(404, true, "Target user not found");
+
+    sendResponse(404, true, "Target user not found", [], [
+        "audit" => $auditObj
+    ]);
 }
 
 if(!isset($body["permission"])) {
-    sendResponse(400, true, "permission string not specified");
+    sendResponse(400, true, "permission string not specified", [], [
+        "audit" => $auditObj
+    ]);
 }
 
-validatePermissionString($body["permission"]);
+$validatePermissionString($body["permission"]);
 
 
 if($user->getUsername() !== "root") {
-    sendResponse(403, true, "Only root can change permissions");
+    $msg = "Only root can change permissions";
+    sendResponse(403, true, "Only root can change permissions", [], [
+        "audit" => $auditObj
+    ]);
 } else {
     if($targetUser->getUsername() === "root") {
-        sendResponse(400, true, "Root permissions cannot be changed");
+        sendResponse(400, true, "Root permissions cannot be changed", [], [
+            "audit" => $auditObj
+        ]);
     }
 
     $permissionObj = \Buildings\PermissionQuery::create()->findOneByAdminId($targetUser->getId());
@@ -60,6 +78,8 @@ if($user->getUsername() !== "root") {
     sendResponse(200, false, $targetUser->getUsername() . " permissions changed successfully", [
         "user" => $targetUser->getUsername(),
         "permission" => $permissionObj->toArray()
+    ], [
+        "audit" => $auditObj
     ]);
 
 
