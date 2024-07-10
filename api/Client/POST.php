7b2c28ce5e9845326515501ab8d62b1ba64d0954
redlintles <treelands54@gmail.com>
2024-07-10 20:49:50 -0300

    Adicionada auditoria em Client POST

--- api/Client/POST.php
+++ api/Client/POST.php
@@ -6,19 +6,27 @@ require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/SendResponse.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/PermissionValidator.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/bodyParser.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/DataValidation.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/Audit.php";
 
 
 $apiKey = $_SERVER["HTTP_X_API_KEY"];
 $transaction = Propel::getConnection(\Buildings\Map\ClientTableMap::DATABASE_NAME);
 
+$auditObj = new AuditObj($apiKey, "CREATE", $request);
+
+$auditObj->setOperation("AddClient");
+
 permissionValidator($apiKey, "CREATE");
+
 
 $body = bodyParser();
 
 $keys = ["username", "password", "email", "phone_number"];
 
 if(array_keys($body) !== $keys) {
-    sendResponse(400, true, "Client data not specified, body must contain username,password,email and phone_number");
+    sendResponse(400, true, "Client data not specified, body must contain username,password,email and phone_number", [], [
+        "audit" => $auditObj
+    ]);
 }
 
 $validateUsername($body["username"]);
@@ -39,14 +47,10 @@ $newClient->setPhoneNumber($body["phone_number"]);
 $usernameExists = \Buildings\ClientQuery::create()->filterByUsername($body["username"])->find();
 
 if(!$usernameExists->isEmpty()) {
-    sendResponse(400, true, "Username already exists, choose another");
+    sendResponse(400, true, "Username already exists, choose another", [], [
+        "audit" => $auditObj
+    ]);
 }
-
-
-
-
-
-
 
 if((bool)$newClient->save()) {
 
@@ -54,10 +58,10 @@ if((bool)$newClient->save()) {
 
 
     if(!isset($user)) {
-        sendResponse(500, true, "Ocorreu um erro");
+        sendResponse(500, true, "An unexpected error ocurred, try again later", [], [
+            "audit" => $auditObj
+        ]);
     }
-
-
 
     $cart = new \Buildings\Cart();
 
@@ -65,12 +69,18 @@ if((bool)$newClient->save()) {
 
     if((bool)$cart->save()) {
         $transaction->commit();
-        sendResponse(200, false, "Client created successfully", ["client" => $newClient->toArray()]);
+        sendResponse(200, false, "Client created successfully", ["client" => $newClient->toArray()], [
+            "audit" => $auditObj
+        ]);
     } else {
         $transaction->rollBack();
-        sendResponse(500, true, "An unexpected error ocurred, try again later");
+        sendResponse(500, true, "An unexpected error ocurred, try again later", [], [
+            "audit" => $auditObj
+        ]);
     }
 } else {
     $transaction->rollBack();
-    sendResponse(500, true, "An unexpected error ocurred, try again later");
+    sendResponse(500, true, "An unexpected error ocurred, try again later", [], [
+        "audit" => $auditObj
+    ]);
 }
