eac1e043642b36aab2d884229988e93c79157e20
redlintles <treelands54@gmail.com>
2024-07-10 20:53:40 -0300

    Adicionada Auditoria em Client UPDATE

--- api/Client/UPDATE.php
+++ api/Client/UPDATE.php
@@ -4,6 +4,7 @@ require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/bodyParser.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/DataValidation.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/PermissionValidator.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/SendResponse.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/Audit.php";
 
 $apiKey = $_SERVER["HTTP_X_API_KEY"];
 
@@ -11,6 +12,9 @@ permissionValidator($apiKey, "UPDATE");
 
 $body = bodyParser();
 
+$auditObj = new AuditObj($apiKey, "UPDATE", $request);
+
+$auditObj->setOperation("UpdateClient");
 $targetClient = null;
 
 
@@ -21,7 +25,9 @@ if(isset($body["client_id"])) {
 
 
 if(!isset($targetClient)) {
-    sendResponse(400, true, "User not found, is client_id specified?");
+    sendResponse(400, true, "User not found, is client_id specified?", [], [
+        "audit" => $auditObj
+    ]);
 }
 
 if(isset($body["username"])) {
@@ -29,7 +35,9 @@ if(isset($body["username"])) {
     $isUnique = \Buildings\ClientQuery::create()->findByUsername($body["username"]);
 
     if(!$isUnique->isEmpty()) {
-        sendResponse(400, true, "Username already exists");
+        sendResponse(400, true, "Username already exists", [], [
+            "audit" => $auditObj
+        ]);
     }
     $targetClient->setUsername($body["username"]);
 }
@@ -47,7 +55,11 @@ if(isset($body["phone_number"])) {
 }
 
 if((bool)$targetClient->save()) {
-    sendResponse(200, false, "Client updated successfully", ["client" => $targetClient->toArray()]);
+    sendResponse(200, false, "Client updated successfully", ["client" => $targetClient->toArray()], [
+        "audit" => $auditObj
+    ]);
 } else {
-    sendResponse(500, true, "An unexpected error ocurred, try again later");
+    sendResponse(500, true, "An unexpected error ocurred, try again later", [], [
+        "audit" => $auditObj
+    ]);
 }
