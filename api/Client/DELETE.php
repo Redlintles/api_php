503b10334be6d809258e08fbacc1be1849df3a35
redlintles <treelands54@gmail.com>
2024-07-10 20:56:24 -0300

    Adicionada Auditoria em Client DELETE

--- api/Client/DELETE.php
+++ api/Client/DELETE.php
@@ -5,6 +5,7 @@ require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/bodyParser.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/SendResponse.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/DataValidation.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/PermissionValidator.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/Audit.php";
 
 $apiKey = $_SERVER["HTTP_X_API_KEY"];
 
@@ -12,6 +13,9 @@ permissionValidator($apiKey, "DELETE");
 
 
 $body = bodyParser();
+
+$auditObj = new AuditObj($apiKey, "DELETE", $request);
+$auditObj->setOperation("DeleteClient");
 
 
 $targetClient = null;
@@ -25,12 +29,18 @@ if(isset($body["client_id"])) {
 }
 
 if(!isset($targetClient)) {
-    sendResponse(400, true, "User not found, is client_id or username specified?");
+    sendResponse(400, true, "User not found, is client_id or username specified?", [], [
+        "audit" => $auditObj
+    ]);
 }
 
 $targetClient->delete();
 if($targetClient->isDeleted()) {
-    sendResponse(200, false, "User deleted successfully", ["user" => $targetClient->toArray()]);
+    sendResponse(200, false, "User deleted successfully", ["user" => $targetClient->toArray()], [
+        "audit" => $auditObj
+    ]);
 } else {
-    sendResponse(500, true, "An unexpected error ocurred, try again later");
+    sendResponse(500, true, "An unexpected error ocurred, try again later", [], [
+        "audit" => $auditObj
+    ]);
 }
