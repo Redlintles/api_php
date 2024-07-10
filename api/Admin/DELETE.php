d94b04738f61008aa959ac2e114bb9aa5af17f9d
redlintles <treelands54@gmail.com>
2024-07-10 20:46:02 -0300

    Adicionada Auditoria em Admin DELTE

--- api/Admin/DELETE.php
+++ api/Admin/DELETE.php
@@ -5,6 +5,9 @@ require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/SendResponse.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/PermissionValidator.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/bodyParser.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/DataValidation.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/Audit.php";
+
+
 $apiKey = $_SERVER["HTTP_X_API_KEY"];
 
 permissionValidator($apiKey, "DELETE");
@@ -13,6 +16,8 @@ permissionValidator($apiKey, "DELETE");
 $body = bodyParser();
 
 $user = \Buildings\AdminQuery::create()->findOneByApiKey($apiKey);
+$auditObj = new AuditObj($apiKey, "DELETE", $request);
+$auditObj->setOperation("DeleteAdmin");
 
 if($user->getUsername() === "root") {
 
@@ -27,15 +32,23 @@ if($user->getUsername() === "root") {
     }
 
     if(!isset($targetUser)) {
-        sendResponse(400, true, "User not found");
+        sendResponse(400, true, "User not found", [], [
+            "audit" => $auditObj
+        ]);
     }
 
     if($targetUser->getUsername() === "root") {
-        sendResponse(400, true, "Root cannot be deleted");
+        sendResponse(400, true, "Root cannot be deleted", [], [
+            "audit" => $auditObj
+        ]);
     }
 
     $targetUser->delete();
-    sendResponse(200, false, "User " . $targetUser->getUsername() . " Deleted successfully");
+    sendResponse(200, false, "User " . $targetUser->getUsername() . " Deleted successfully", [], [
+        "audit" => $auditObj
+    ]);
 } else {
-    sendResponse(403, true, "Only root can delete admins");
+    sendResponse(403, true, "Only root can delete admins", [], [
+        "audit" => $auditObj
+    ]);
 }
