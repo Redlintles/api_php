40b221d76361cda97ef83612c00810ead1ae39e3
redlintles <treelands54@gmail.com>
2024-07-10 20:24:38 -0300

    Adicionada Auditoria em Admin GET

--- api/Admin/GET.php
+++ api/Admin/GET.php
@@ -6,6 +6,7 @@ require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/SendResponse.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/bodyParser.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/CollectionToArray.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/DataValidation.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/Audit.php";
 
 
 $apiKey = $_SERVER["HTTP_X_API_KEY"];
@@ -13,6 +14,8 @@ $apiKey = $_SERVER["HTTP_X_API_KEY"];
 permissionValidator($apiKey, "READ");
 
 $body = bodyParser();
+$auditObj = new AuditObj($apiKey, "READ", $request);
+$auditObj->setOperation("ReadAdmin");
 
 $user = \Buildings\AdminQuery::create()->findOneByApiKey($apiKey);
 
@@ -33,12 +36,20 @@ if(isset($user) && $user->getUsername() === "root") {
         $users = \Buildings\AdminQuery::create()->find();
 
         $resultUsers = collectionToArray($users);
-        sendResponse(200, false, "No criteria specified(admin_id,username) fetching all admins", ["users" => $resultUsers]);
+        sendResponse(200, false, "No criteria specified(admin_id,username) fetching all admins", ["users" => $resultUsers], [
+            "audit" => $auditObj
+        ]);
     } else {
-        sendResponse(200, false, "Data from " . $targetUser->getUsername() . " Fetched successfully", $targetUser->toArray());
+        sendResponse(200, false, "Data from " . $targetUser->getUsername() . " Fetched successfully", $targetUser->toArray(), [
+            "audit" => $auditObj
+        ]);
     }
 } elseif(isset($user)) {
-    sendResponse(200, false, "User data fetched successfully", $user->toArray());
+    sendResponse(200, false, "User data fetched successfully", $user->toArray(), [], [
+        "audit" => $auditObj
+    ]);
 } else {
-    sendResponse(404, true, "User does not exists");
+    sendResponse(404, true, "User does not exists", [], [
+        "audit" => $auditObj,
+    ]);
 }
