733f58529ebe1793feba4c15f8fcf74a9314638e
redlintles <treelands54@gmail.com>
2024-07-10 18:59:56 -0300

    Modificado funcionamento da função de auditoria

--- functions/Audit.php
+++ functions/Audit.php
@@ -1,13 +1,17 @@
 <?php
 
-function audit(string $apiKey, string $type, string $route, string $data_str)
+function audit(string $apiKey, string $type, string $route, int $httpCode, string $operation, bool $success, string $data = "")
 {
     $audit = new \Buildings\Audit();
 
+    $additionalData = ($data ? "/" . $data : "");
+
+    $dataString = ucfirst(explode("/", $type)[1]) . "/" . $operation . "/" . ($success ? "success" : "error") . "/" . $httpCode . $additionalData;
     $username = \Buildings\AdminQuery::create()->findOneByApiKey($apiKey)->getUsername();
     $audit->setOperationExecutor($username);
     $audit->setOperationType($type);
     $audit->setOperationRoute($route);
-    $audit->setOperationDataString($data_str);
+    $audit->setOperationDataString($dataString);
+
     $audit->save();
 }
