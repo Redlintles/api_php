d3f139d3e9063a9ec399567c9ba33eec5199a1a7
redlintles <treelands54@gmail.com>
2024-07-10 21:08:14 -0300

    Adicionada Auditoria em Address DELETE

--- api/Address/DELETE.php
+++ api/Address/DELETE.php
@@ -5,12 +5,17 @@ require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/SendResponse.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/DataValidation.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/bodyParser.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/PermissionValidator.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/Audit.php";
 
 $apiKey = $_SERVER["HTTP_X_API_KEY"];
 
 permissionValidator($apiKey, "DELETE");
 
 $body = bodyParser();
+
+$auditObj = new AuditObj($apiKey, "DELETE", $request);
+
+$auditObj->setOperation("DeleteAddress");
 
 
 $targetAddress = null;
@@ -21,12 +26,18 @@ if($body["address_id"]) {
 }
 
 if(!isset($targetAddress)) {
-    sendResponse(400, true, "Target address not found, is address_id set?");
+    sendResponse(400, true, "Target address not found, is address_id set?", [], [
+        "audit" => $auditObj
+    ]);
 }
 
 $targetAddress->delete();
 if($targetAddress->isDeleted()) {
-    sendResponse(200, false, "Address Deleted Successfully");
+    sendResponse(200, false, "Address Deleted Successfully", [], [
+        "audit" => $auditObj
+    ]);
 } else {
-    sendResponse(500, true, "An unexpected error ocurred, try again later");
+    sendResponse(500, true, "An unexpected error ocurred, try again later", [], [
+        "audit" => $auditObj
+    ]);
 }
