ab56bfce933383cb9fb1be55efa0983c8907f673
redlintles <treelands54@gmail.com>
2024-07-12 15:54:49 -0300

    Adicionado opção de retorno null a FIndSingle

--- functions/FindSingle.php
+++ functions/FindSingle.php
@@ -1,6 +1,6 @@
 <?php
 
-
+use Symfony\Component\Validator\Constraints\Optional;
 
 function findSingle(array $body, array $options)
 {
@@ -8,13 +8,16 @@ function findSingle(array $body, array $options)
     require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/snakeToCamel.php";
     $targetObj = null;
 
+    if(!isset($options["throw_error"])) {
+        $options["throw_error"] = true;
+    }
 
     foreach($options["keys"] as $key => $validation) {
         if(isset($body[$key])) {
             $validation($body[$key]);
             $methodName = "findOneById";
             if(!preg_match("/_id/", $key)) {
-                $methodName = "findOneBy" . ucfirst(snakeToCamel($body[$key]));
+                $methodName = "findOneBy" . ucfirst(snakeToCamel($key));
             }
             $targetObj = $options["query"]->$methodName($body[$key]);
         }
@@ -23,10 +26,12 @@ function findSingle(array $body, array $options)
         }
     }
 
-    if(!isset($targetObj)) {
+    if(!isset($targetObj) && $options["throw_error"]) {
         sendResponse(400, true, "Object not found, have you set any of " . implode(",", $options["keys"]) . "?", [], [
             $audit => $options["audit"]
         ]);
+    } elseif(!isset($targetObj)) {
+        return null;
     } else {
         return $targetObj;
     }
