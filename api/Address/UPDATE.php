2a7ca62d9c09e62d0f32c41e4639c05fdd63ebef
redlintles <treelands54@gmail.com>
2024-07-12 15:41:27 -0300

    Código de Address Update Refatorado

--- api/Address/UPDATE.php
+++ api/Address/UPDATE.php
@@ -1,75 +1,60 @@
 <?php
 
-
 require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/bodyParser.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/DataValidation.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/SendResponse.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/PermissionValidator.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/Audit.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/FindSingle.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/groupValidation.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/UpdateObject.php";
 
 $apiKey = $_SERVER["HTTP_X_API_KEY"];
 
 $body = bodyParser();
 
-
 $auditObj = new AuditObj($apiKey, "UPDATE", $request);
-
 $auditObj->setOperation("UpdateAddress");
 
 permissionValidator($apiKey, "UPDATE");
 
-$targetAddress = null;
-
-if(isset($body["address_id"])) {
-    $validateInteger($body["address_id"]);
-    $targetAddress = \Buildings\AddressQuery::create()->findOneById($body["address_id"]);
-}
-
-if(!isset($targetAddress)) {
-    sendResponse(400, true, "address could not be found, is address_id set?", [], [
-        "audit" => $auditObj
-    ]);
-}
+$targetAddress = findSingle($body, [
+    "keys" => [
+        "address_id" => $validateInteger,
+    ],
+    "audit" => $auditObj,
+    "query" => \Buildings\AddressQuery::create()
+]);
 
 
-$mergeObj = [];
+$mergeObj = groupValidation($body, [
+    "audit" => $auditObj,
+    "keys" => [
+        "country?" => $validateCapitalized,
+        "state?" => $validateState,
+        "city?" => $validateCapitalized,
+        "neighborhood?" => $validateCapitalized,
+        "street?" => $validateCapitalized,
+        "house_number?" => $validateHouseNumber
+    ],
+    "at_least" => 1
+]);
 
-if($body["country"]) {
-    $validateCapitalized($body["country"]);
-    $mergeObj["country"] = $body["country"];
-}
-if($body["state"]) {
-    $validateState($body["state"]);
-    $mergeObj["state"] = $body["state"];
-}
-if($body["city"]) {
-    $validateCapitalized($body["city"]);
-    $mergeObj["city"] = $body["city"];
-}
-if($body["neighborhood"]) {
-    $validateCapitalized($body["neighborhood"]);
-    $mergeObj["neighborhood"] = $body["neighborhood"];
-}
-if($body["street"]) {
-    $validateCapitalized($body["street"]);
-    $mergeObj["street"] = $body["street"];
-}
-if($body["house_number"]) {
-    $validateHouseNumber($body["house_number"]);
-    $mergeObj["number"] = $body["house_number"];
+if(isset($mergeObj["house_number"])) {
+    $mergeObj["number"] = $mergeObj["house_number"];
+    unset($mergeObj["house_number"]);
 }
 
-foreach($mergeObj as $key => $value) {
-    $methodName = "set" . ucfirst($key);
-    $targetAddress->$methodName($value);
-}
-
-if((bool)$targetAddress->save()) {
-    sendResponse(200, false, "Object updated successfully", [], [
-        "audit" => $auditObj
-    ]);
-} else {
-    sendResponse(500, true, "An unexpected error ocurred, try again later", [], [
-        "audit" => $auditObj
-    ]);
-}
+UpdateObject($body, [
+    "country" => $validateCapitalized,
+    "state" => $validateState,
+    "city" => $validateCapitalized,
+    "neighborhood" => $validateCapitalized,
+    "street" => $validateCapitalized,
+    "house_number" => $validateHouseNumber
+], $targetAddress, [
+    "audit" => $auditObj,
+    "result" => "address",
+    "success_msg" => "Address Updated Successfully",
+    "query" => \Buildings\AddressQuery::create(),
+]);
