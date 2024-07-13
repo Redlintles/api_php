892dea5ab006c89798f0cfb9bc38c467638d64d4
redlintles <treelands54@gmail.com>
2024-07-13 12:44:53 -0300

    Adicionado caso de erro em bodyParser para body vazio

--- functions/bodyParser.php
+++ functions/bodyParser.php
@@ -1,8 +1,14 @@
 <?php
 
 
+require_once __DIR__ . "/sendResponse.php";
 function bodyParser()
 {
     $json = file_get_contents("php://input");
-    return json_decode($json, true);
+    $result = json_decode($json, true);
+    if(!isset($result)) {
+        sendResponse(400, true, "Request body is not set, it must be a JSON", [], []);
+    } else {
+        return $result;
+    }
 }
