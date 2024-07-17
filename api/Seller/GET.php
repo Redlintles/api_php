1fea9a6147d7eeca9d174fe04b4a49731e4abb6c
redlintles <treelands54@gmail.com>
2024-07-17 18:09:33 -0300

    Adicionado Endpoint Seller GET

---
+++ api/Seller/GET.php
@@ -0,0 +1,53 @@
+<?php
+
+
+
+require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/bodyParser.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/audit.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/findSingle.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/permissionValidator.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/bodyParser.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/groupValidation.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/sendResponse.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/dataValidation.php";
+
+$apiKey = $_SERVER["HTTP_X_API_KEY"];
+
+permissionValidator($apiKey, "READ");
+
+$auditObj = new AuditObj($apiKey, "READ", $request);
+$auditObj->setOperation("GetSeller");
+
+$body = bodyParser();
+
+$body = groupValidation($body, [
+    "keys" => [
+        "seller_id?" => $validateInteger,
+        "username?" => $validateUsername
+    ],
+    "at_least" => 1,
+    "audit" => $auditObj
+]);
+
+
+$targetSeller = findSingle($body, [
+    "keys" => [
+        "seller_id" => $validateInteger,
+        "username" => $validateUsername
+    ],
+    "query" => \Buildings\SellerQuery::create(),
+    "audit" => $auditObj
+]);
+
+
+if(isset($targetSeller)) {
+    sendResponse(200, false, "Seller fetched successfully", [
+        "seller" => $targetSeller->toArray()
+    ], [
+        "audit" => $auditObj
+    ]);
+} else {
+    sendResponse(500, true, "An unexpected error ocurred, try again later", [], [
+        "audit" => $auditObj
+    ]);
+}
