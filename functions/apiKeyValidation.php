8563d9ecb32687dcc80eaedc027dc9d7efac44d7
redlintles <treelands54@gmail.com>
2024-07-17 14:46:38 -0300

    Adicionada verificação de criptografia a apiKeyValidation

--- functions/apiKeyValidation.php
+++ functions/apiKeyValidation.php
@@ -7,10 +7,22 @@ function apiKeyValidator()
     if (isset($_SERVER["HTTP_X_API_KEY"])) {
         $apiKey = $_SERVER["HTTP_X_API_KEY"];
 
-        // Realiza a consulta no banco de dados para verificar a chave API
-        $result = AdminQuery::create()->findOneByApiKey($apiKey);
+        $userId = -1;
 
-        if (!$result) {
+        $users = AdminQuery::create()->find();
+
+        foreach($users as $user) {
+            $encrypted = $user->getApiKey();
+            if(password_verify($apiKey, $encrypted)) {
+                $userId = $user->getId();
+            }
+        }
+
+
+        // Realiza a consulta no banco de dados para verificar a chave API
+
+
+        if ($userId === -1) {
             return false; // Chave API não encontrada ou inválida
         } else {
             return true; // Chave API válida
