11e0cf7c8a9bb67112113391711929e601098b6c
redlintles <treelands54@gmail.com>
2024-07-17 21:58:29 -0300

    Adicionado endpoint Discount DELETE

---
+++ api/Discount/DELETE.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/sendResponse.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/findSingle.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/dataValidation.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/audit.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/bodyParser.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/permissionValidator.php";
+
+$apiKey = $_SERVER["HTTP_X_API_KEY"];
+
+permissionValidator($apiKey, "DELETE");
+
+$auditObj = new AuditObj($apiKey, "DELETE", $request);
+$auditObj->setOperation("DeleteDiscount");
+
+$body = bodyParser();
+
+$targetDiscount = findSingle($body, [
+    "keys" => [
+        "discount_id" => $validateInteger
+    ],
+    "audit" => $auditObj,
+    "query" => \Buildings\DiscountQuery::create()
+]);
+
+$targetDiscount->delete();
+
+if($targetDiscount->isDeleted()) {
+    sendResponse(
+        200,
+        false,
+        "Discount deleted successfully",
+        [
+        "discount" => $targetDiscount->toArray()
+    ],
+        [
+        "audit" => $auditObj
+    ]
+    );
+} else {
+    sendResponse(500, true, "An unexpected error ocurred, try again later", [], [
+        "audit" => $auditObj
+    ]);
+}
--- api/Discount/router.php
+++ api/Discount/router.php
@@ -3,4 +3,6 @@
 
 if($_SERVER["REQUEST_METHOD"] === "POST") {
     require_once __DIR__ . "/POST.php";
+} elseif($_SERVER["REQUEST_METHOD"] === "DELETE") {
+    require_once __DIR__ . "/DELETE.php";
 }
