c6fa004ebc7dead0b5503c8536ccda9c6fb803b6
redlintles <treelands54@gmail.com>
2024-07-18 22:11:44 -0300

    Criada classe Validate para resolver problemas de escopo com closures dentro de funções

--- functions/dataValidation.php
+++ functions/dataValidation.php
@@ -19,6 +19,151 @@ function validationFactory(string $regex, string $message)
     };
 }
 
+
+class Validate
+{
+    private static function validationFactory(string $regex, string $message)
+    {
+        return function (string | array $toValidate) use ($regex, $message) {
+            if(is_array($toValidate)) {
+                foreach($toValidate as $item) {
+                    if(!preg_match($regex, $item)) {
+                        sendResponse(400, true, $message);
+                    }
+                }
+            } else {
+                if(!preg_match($regex, $toValidate)) {
+                    sendResponse(400, true, $message);
+                }
+            }
+        };
+    }
+
+
+    public static function validateUsername(string $data)
+    {
+        $__validateUsername = self::validationFactory(...[
+            "/^[a-zA-Z]{8,46}(?:\d{0,4}?)$/",
+            "Invalid Username, length must be in range(8,50) and it should not start with a number"
+        ]);
+        $__validateUsername($data);
+        return true;
+
+    }
+
+    public static function validateEmail(string $data)
+    {
+        $__validateEmail = self::validationFactory(...[
+            "/^[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]+$/",
+            "Invalid Email(banana@gmail.com)"
+        ]);
+        $__validateEmail($data);
+        return true;
+    }
+    public static function validatePassword(string $data)
+    {
+
+        $__validatePassword = self::validationFactory(...[
+            "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,30}$/",
+            "Invalid Password, length must be in range(8,30) and it should contain at least one uppercase letter, one lowercase, one number and one special character"
+
+        ]);
+        $__validatePassword($data);
+        return true;
+    }
+    public static function validatePhoneNumber(string $data)
+    {
+
+        $__validatePhoneNumber = self::validationFactory(...[
+                "/^[0-9]{11,15}$/",
+                "Invalid Phone Number, min is 11 digits and max is 15"
+        ]);
+        $__validatePhoneNumber($data);
+        return true;
+    }
+    public static function validatePermissionString(string $data)
+    {
+
+        $__validatePermissionString = self::validationFactory(...[
+                "/^[0-1]{4}$/",
+                "Invalid permission string, it should look like as '1010'"
+        ]);
+        $__validatePermissionString($data);
+        return true;
+    }
+    public static function validateInteger(string $data)
+    {
+
+        $__validateInteger = self::validationFactory(...[
+                "/^\d+$/",
+                "Invalid Input (must be an integer)"
+        ]);
+        $__validateInteger($data);
+        return true;
+    }
+    public static function validateState(string $data)
+    {
+
+        $__validateState = self::validationFactory(...[
+                "/^[A-Z]{2}$/",
+                "Invalid state(must be a capitalized string"
+        ]);
+        $__validateState($data);
+        return true;
+    }
+    public static function validateHouseNumber(string $data)
+    {
+
+        $__validateHouseNumber = self::validationFactory(...[
+                "/^[A-Za-z0-9\-\/\s]{1,10}$/",
+                "Invalid houseNumber(must be a capitalized string"
+        ]);
+        $__validateHouseNumber($data);
+        return true;
+    }
+    public static function validateCapitalized(string $data)
+    {
+
+        $__validateCapitalized = self::validationFactory(...[
+               "/^(?:[A-Z][a-z]+\s?)+$/",
+                "Invalid Capitalized String(must be a capitalized string)"
+        ]);
+        $__validateCapitalized($data);
+        return true;
+    }
+    public static function validateUnityPrice(string $data)
+    {
+
+        $__validateUnityPrice = self::validationFactory(...[
+                "/^[0-9]+\.[0-9]{2}$/",
+                "Invalid Unity Price format is (10.99)"
+        ]);
+        $__validateUnityPrice($data);
+        return true;
+    }
+    public static function validateDate(string $data)
+    {
+
+        $__validateDate = self::validationFactory(...[
+            "/^[0-9]{4}-[0-9]{2}-[0-9]{2}\s[0-9]{2}:[0-9]{2}:[0-9]{2}$/",
+            "Invalid date, must be in format '2024-01-01 23:59:02' "
+        ]);
+        $__validateDate($data);
+        return true;
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
 $validateUsername = validationFactory(
     "/^[a-zA-Z]{8,46}(?:\d{0,4}?)$/",
     "Invalid Username, length must be in range(8,50) and it should not start with a number"
