8d3c08d6b3b444c0a6e960fd21094f68f9a1c895
redlintles <treelands54@gmail.com>
2024-12-14 21:42:36 -0300

    Removidos imports desnecessários de Client Post

--- api/Client/POST.php
+++ api/Client/POST.php
@@ -2,14 +2,6 @@
 
 use Propel\Runtime\Propel;
 
-require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/sendResponse.php";
-require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/permissionValidator.php";
-require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/bodyParser.php";
-require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/dataValidation.php";
-require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/audit.php";
-require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/groupValidation.php";
-require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/verifyUnicity.php";
-
 $apiKey = $_SERVER["HTTP_X_API_KEY"];
 $transaction = Propel::getConnection(\Buildings\Map\ClientTableMap::DATABASE_NAME);
 
