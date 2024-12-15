49bc3ec4125a9373cbc97d88bb1eb4b65a149a5d
redlintles <treelands54@gmail.com>
2024-12-14 21:42:00 -0300

    Adicionado campo discount_price em GET.php e toda lógica referente a ele, código formatado

--- api/Product/GET.php
+++ api/Product/GET.php
@@ -7,6 +7,7 @@ require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/audit.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/permissionValidator.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/dataValidation.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/findSingle.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/discount.php";
 
 
 $apiKey = $_SERVER["HTTP_X_API_KEY"];
@@ -17,21 +18,26 @@ $auditObj = new AuditObj($apiKey, "READ", $request);
 $auditObj->setOperation("ReadProduct");
 $body = bodyParser();
 
+
+
 $targetProduct = findSingle($body, [
     "audit" => $auditObj,
-    "keys" => ["product_id" => $validateInteger,"title" => $validateCapitalized],
+    "keys" => ["product_id" => 'validateInteger',"title" => 'validateCapitalized'],
     "query" => \Buildings\ProductQuery::create(),
 ]);
 
 $categories = \Buildings\ProductCategoryQuery::create()->findByIdProduct($targetProduct);
 
 $productCategories = [];
-foreach($categories as $c) {
+foreach ($categories as $c) {
     $category = \Buildings\CategoryQuery::create()->findOneById($c->getIdCategory());
     array_push($productCategories, $category->getName());
 }
 
 
-sendResponse(200, false, "Product found successfully", ["product" => $targetProduct->toArray(), "categories" => $productCategories], [
+$discountedPrice = getDiscountsByFinalPrice($targetProduct, $productCategories);
+
+
+sendResponse(200, false, "Product found successfully", ["product" => $targetProduct->toArray(), "categories" => $productCategories, "discounted_rpice" => $discountedPrice], [
     "audit" => $auditObj
 ]);
