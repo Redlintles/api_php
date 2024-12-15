f5017db05a6a9240e9687207d88fb5e7a0e3915b
redlintles <treelands54@gmail.com>
2024-12-14 21:38:58 -0300

    Corrigido findSingle para chamar Validate

--- functions/findSingle.php
+++ functions/findSingle.php
@@ -15,30 +15,32 @@ function findSingle(array $body, array $options)
     require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/snakeToCamel.php";
     $targetObj = null;
 
-    if(!isset($options["throw_error"])) {
+    if (!isset($options["throw_error"])) {
         $options["throw_error"] = true;
     }
 
-    foreach($options["keys"] as $key => $validation) {
-        if(isset($body[$key])) {
-            $validation($body[$key]);
+    foreach ($options["keys"] as $key => $validation) {
+        if (isset($body[$key])) {
+
+
+            Validate::$validation($body[$key]);
             $methodName = "findOneById";
-            if(!preg_match("/_id/", $key)) {
+            if (!preg_match("/_id/", $key)) {
                 $methodName = "findOneBy" . ucfirst(snakeToCamel($key));
             }
             $targetObj = $options["query"]->$methodName($body[$key]);
         }
-        if(isset($targetObj)) {
+        if (isset($targetObj)) {
             break;
         }
     }
 
-    if(!isset($targetObj) && $options["throw_error"]) {
+    if (!isset($targetObj) && $options["throw_error"]) {
         sendResponse(400, true, "Object not found, have you set any of " . implode(",", array_keys($options["keys"])) . "?", [], [
             $audit => $options["audit"]
         ]);
         throw new Exception("Object not found");
-    } elseif(!isset($targetObj)) {
+    } elseif (!isset($targetObj)) {
         return null;
     }
     return $targetObj;
