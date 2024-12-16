27f964529566c4238be2649aacf8409811133dbc
redlintles <treelands54@gmail.com>
2024-12-16 19:10:51 -0300

    Adicionados docBlocks em router

--- functions/router.php
+++ functions/router.php
@@ -1,11 +1,29 @@
 <?php
 
+
+/**
+ * A Router class to control and redirect requests in a easier way
+ */
 class Router
 {
+    /**
+     * @var array $routes stores the routes of the Router
+     */
     private array $routes;
+    /**
+     * @var string $listenFor The route path to listen for
+     */
     public string $listenFor;
+    /**
+     * @var string $root the Root dir of the files which will be targeted by the router
+     */
     public string $root;
 
+    /**
+     * Initializes the router
+     * @param string $listenFor the route path to listen for
+     * @param string $root the Root dir of the files which will be targeted by the router
+     */
     public function __construct(string $listenFor, string $root)
     {
         $this->listenFor = $listenFor;
@@ -13,6 +31,10 @@ class Router
         $this->routes = [];
     }
 
+    /**
+     * Calls the file which matches request
+     * @param string $request A request string for the router to redirect
+     */
     public function call(string $request)
     {
 
@@ -31,7 +53,11 @@ class Router
             }
         }
     }
-
+    /**
+     * Maps a post request to a file which will handle it
+     * @param string $route The route to redirect
+     * @param string $file The file which the route will be redirected to, must start with a slash
+     */
     public function post(string $route, string $file)
     {
         $routeToPush = [
@@ -42,6 +68,11 @@ class Router
 
         array_push($this->routes, $routeToPush);
     }
+    /**
+     * Maps a get request to a file which will handle it
+     * @param string $route The route to redirect
+     * @param string $file The file which the route will be redirected to, must start with a slash
+     */
     public function get(string $route, string $file)
     {
         $routeToPush = [
@@ -52,6 +83,11 @@ class Router
 
         array_push($this->routes, $routeToPush);
     }
+    /**
+     * Maps a put request to a file which will handle it
+     * @param string $route The route to redirect
+     * @param string $file The file which the route will be redirected to, must start with a slash
+     */
     public function put(string $route, string $file)
     {
         $routeToPush = [
@@ -62,6 +98,11 @@ class Router
 
         array_push($this->routes, $routeToPush);
     }
+    /**
+     * Maps a delete request to a file which will handle it
+     * @param string $route The route to redirect
+     * @param string $file The file which the route will be redirected to, must start with a slash
+     */
     public function delete(string $route, string $file)
     {
         $routeToPush = [
