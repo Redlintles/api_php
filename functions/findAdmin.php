53d40e5e3db6dfa72b0958900d4f644388f8cdba
redlintles <treelands54@gmail.com>
2024-12-16 18:45:57 -0300

    Adicionados docBlocks a findAdmin

--- functions/findAdmin.php
+++ functions/findAdmin.php
@@ -1,22 +1,25 @@
 <?php
 
-
-
 require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/sendResponse.php";
-function findAdmin(string $apiKey)
+
+/**
+ * @param string $apiKey The api key belonging to the admin
+ * @return \Buildings\Admin The returned admin
+ */
+function findAdmin(string $apiKey): \Buildings\Admin
 {
     $admins = \Buildings\AdminQuery::create()->find();
 
     $adm = null;
-    foreach($admins as $admin) {
+    foreach ($admins as $admin) {
         $encrypted = $admin->getApiKey();
-        if(password_verify($apiKey, $encrypted)) {
+        if (password_verify($apiKey, $encrypted)) {
             $adm = $admin;
             break;
         }
     }
 
-    if(!isset($adm)) {
+    if (!isset($adm)) {
         sendResponse(403, true, "Admin not found");
     } {
         return $adm;
