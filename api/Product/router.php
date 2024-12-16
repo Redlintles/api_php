b7a59b80269cb18963732b56defde40054bfd441
redlintles <treelands54@gmail.com>
2024-12-16 17:37:00 -0300

    Alterado o nome da rota de categories para UpdateCategories

--- api/Product/router.php
+++ api/Product/router.php
@@ -7,17 +7,6 @@ $router->post("/", "/POST.php");
 $router->get("/", "/GET.php");
 $router->put("/", "/UPDATE.php");
 $router->delete("/", "/DELETE.php");
-$router->put("/categories", "/UpdateCategories.php");
+$router->put("/UpdateCategories", "/UpdateCategories.php");
 
 $router->call($request);
-// if($_SERVER["REQUEST_METHOD"] === "POST") {
-//     require_once __DIR__ . "/POST.php";
-// } elseif($_SERVER["REQUEST_METHOD"] === "GET") {
-//     require_once __DIR__ . "/GET.php";
-// } elseif($request === "/api/product/updateCategories" && $_SERVER["REQUEST_METHOD"] === "PUT") {
-//     require_once __DIR__ . "/UpdateCategories.php";
-// } elseif($_SERVER["REQUEST_METHOD"] === "PUT") {
-//     require_once __DIR__ . "/UPDATE.php";
-// } elseif($_SERVER["REQUEST_METHOD"] === "DELETE") {
-//     require_once __DIR__ . "/DELETE.php";
-// }
