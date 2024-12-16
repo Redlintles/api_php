b5c7546c3ca63ebdbb1ce637a82c571fb481b578
redlintles <treelands54@gmail.com>
2024-12-16 19:12:43 -0300

    Adicionados docBlocks em snakeToCamel

--- functions/snakeToCamel.php
+++ functions/snakeToCamel.php
@@ -1,7 +1,11 @@
 <?php
 
-
-function snakeToCamel($string)
+/**
+ * Converts a snakeCase string to a camelCase string
+ * @param string $string the snakeCase string to convert
+ * @return string The camelCase String
+ */
+function snakeToCamel(string $string): string
 {
     $split = explode("_", strtolower($string));
 
