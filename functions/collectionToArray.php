44179b2e6ed5c12eb68f808d68e4b7b13fc8fec8
redlintles <treelands54@gmail.com>
2024-12-16 18:12:31 -0300

    Adicionados DocBlocks a collectionToArray.php

--- functions/collectionToArray.php
+++ functions/collectionToArray.php
@@ -1,11 +1,14 @@
 <?php
 
-
-function collectionToArray(\Propel\Runtime\Collection\Collection $collection)
+/**
+ * @param Propel\Runtime\Collection\Collection $collection, Converts a propel collection to a PHP Array
+ * @return array The converted collection
+ */
+function collectionToArray(\Propel\Runtime\Collection\Collection $collection): array
 {
     $result = [];
 
-    foreach($collection as $item) {
+    foreach ($collection as $item) {
         array_push($result, $item->toArray());
     }
 
