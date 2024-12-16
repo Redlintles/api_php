00cb7142df2d56d296b0e6ebca2b4500f58a2de7
redlintles <treelands54@gmail.com>
2024-12-16 17:10:29 -0300

    Adicionada Tipagem em dynamicQuery

--- functions/dynamicQuery.php
+++ functions/dynamicQuery.php
@@ -1,11 +1,15 @@
 <?php
 
-
-function dynamicQuery($queryObj, $params)
+/**
+ * @param Propel\Runtime\ActiveQuery\ModelCriteria $queryObj The Object to query
+ * @param array $params The Params for the query
+ * @return Propel\Runtime\Collection\ObjectCollection $ The Query Result;
+ */
+function dynamicQuery(Propel\Runtime\ActiveQuery\ModelCriteria $queryObj, array $params): Propel\Runtime\Collection\ObjectCollection
 {
-    foreach($params as $key => $value) {
+    foreach ($params as $key => $value) {
         $methodName = "filterBy" . ucfirst($key);
-        if(is_array($value)) {
+        if (is_array($value)) {
             $queryObj->$methodName($value, \Propel\Runtime\ActiveQuery\Criteria::IN);
         } else {
             $queryObj->$methodName($value);
