dd7f8923906d4d80be64c8455c973ac63df52429
redlintles <treelands54@gmail.com>
2024-12-16 17:45:32 -0300

    código de Cart router refatorado para utilizar Router

--- api/Cart/router.php
+++ api/Cart/router.php
@@ -1,10 +1,9 @@
 <?php
 
+$router = new Router("/cart", __DIR__);
 
-if($_SERVER["REQUEST_METHOD"] === "GET") {
-    require_once __DIR__ . "/GET.php";
-} elseif($_SERVER["REQUEST_METHOD"] === "POST") {
-    require_once __DIR__ . "/AddCartProduct.php";
-} elseif($_SERVER["REQUEST_METHOD"] === "DELETE") {
-    require_once __DIR__ . "/RemoveCartProduct.php";
-}
+$router->get("/", "/GET.php");
+$router->post("/", "/AddCartProduct.php");
+$router->delete("/", "/RemoveCartProduct.php");
+
+$router->call($request);
