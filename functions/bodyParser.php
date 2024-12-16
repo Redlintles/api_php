f192abce8aebef0f0f0dec74365e6afeab0d45e5
redlintles <treelands54@gmail.com>
2024-12-16 17:59:14 -0300

    Adicionado docBlock À BodyParser

--- functions/bodyParser.php
+++ functions/bodyParser.php
@@ -2,11 +2,14 @@
 
 
 require_once __DIR__ . "/sendResponse.php";
+/**
+ * @return array Decodes the req body as JSON and returns an associative Array
+ */
 function bodyParser()
 {
     $json = file_get_contents("php://input");
     $result = json_decode($json, true);
-    if(!isset($result)) {
+    if (!isset($result)) {
         sendResponse(400, true, "Request body is not set, it must be a JSON", [], []);
     } else {
         return $result;
