ce005374b8696a0a5c0c8804134103e80b53aacf
redlintles <treelands54@gmail.com>
2024-12-17 20:58:06 -0300

    Adicionada rota Order GET

---
+++ api/Order/GET.php
@@ -0,0 +1,25 @@
+<?php
+
+$body = bodyParser();
+
+$apiKey = $_SERVER["HTTP_X_API_KEY"];
+$auditObj = new AuditObj($apiKey, "READ", $request);
+permissionValidator($apiKey, "READ");
+
+$body = groupValidation($body, [
+    "keys" => [
+        "order_id" => "validateInteger",
+    ],
+    "at_least" => 1,
+    "audit" => $auditObj
+]);
+
+$targetOrder = findSingle($body, [
+    "order_id" => "id"
+], \Buildings\OrderQuery::create(), true, $auditObj);
+
+$orderProducts = dynamicQuery(\Buildings\OrderProductQuery::create(), [
+    "id_order" => $targetOrder->getId()
+]);
+
+sendResponse(200, false, "Order fetched successfully", ["order" => $targetOrder->toArray(), "order_products" => collectionToArray($orderProducts)], ["audit" => $auditObj]);
--- api/Order/router.php
+++ api/Order/router.php
@@ -3,6 +3,7 @@
 $router = new Router("/order", __DIR__);
 
 $router->post("/", "/POST.php");
+$router->get("/", "/GET.php");
 
 
 $router->call($request);
