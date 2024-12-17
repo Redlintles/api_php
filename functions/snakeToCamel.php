26dcb62d17865b9781f8a60bbed1dd3f79034fb6
redlintles <treelands54@gmail.com>
2024-12-17 18:20:22 -0300

    Adicionado parâmetro  a snakeToCamel

--- functions/snakeToCamel.php
+++ functions/snakeToCamel.php
@@ -3,15 +3,22 @@
 /**
  * Converts a snakeCase string to a camelCase string
  * @param string $string the snakeCase string to convert
+ * @param bool $lcFirst, default is true, lowerCases the first letter of the result
  * @return string The camelCase String
  */
-function snakeToCamel(string $string): string
+function snakeToCamel(string $string, bool $lcFirst = true): string
 {
     $split = explode("_", strtolower($string));
 
     $split = array_map("ucfirst", $split);
 
-    $result = lcfirst(implode("", $split));
+    $result = implode("", $split);
+
+    if ($lcFirst) {
+        $result = lcfirst($result);
+    } else {
+        $result = ucfirst($result);
+    }
 
     return $result;
 }
