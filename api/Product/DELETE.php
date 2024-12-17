4e0e2808a411cd79264b6a0d34b80ffe0b03be69
redlintles <treelands54@gmail.com>
2024-12-17 19:08:56 -0300

    Corrigido erro na chamada de findSingle em Product DELETE

--- api/Product/DELETE.php
+++ api/Product/DELETE.php
@@ -5,17 +5,12 @@ require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/bodyParser.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/audit.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/permissionValidator.php";
 
-
 $apiKey = $_SERVER["HTTP_X_API_KEY"];
-
-permissionValidator($apiKey, "DELETE");
-
 $body = bodyParser();
-
 $auditObj = new AuditObj($apiKey, "DELETE", $request);
 
+permissionValidator($apiKey, "DELETE");
 $auditObj->setOperation("DeleteProduct");
-
 
 function decrementProduct()
 {
@@ -25,13 +20,8 @@ function decrementProduct()
     global $body,$auditObj;
 
     $targetProduct = findSingle($body, [
-        "query" => \Buildings\ProductQuery::create(),
-        "keys" => [
-            "product_id" => "validateInteger",
-            "title" => "validateCapitalized"
-        ],
-        "audit" => "auditObj"
-    ]);
+        "product_id" => "id",
+    ], \Buildings\ProductQuery::create(), true, $auditObj);
 
     $validateInteger($body["quantity"]);
     $targetProduct->decrementProduct($body["quantity"]);
@@ -46,13 +36,8 @@ function deleteProduct()
     global $body,$auditObj;
 
     $targetProduct = findSingle($body, [
-        "query" => \Buildings\ProductQuery::create(),
-        "keys" => [
-            "product_id" => "validateInteger",
-            "title" => "validateCapitalized"
-        ],
-        "audit" => "auditObj"
-    ]);
+        "product_id" => "id",
+    ], \Buildings\ProductQuery::create(), true, $auditObj);
 
     $targetProduct->delete();
 
@@ -66,9 +51,7 @@ function deleteProduct()
 }
 
 if (!isset($body["type"])) {
-
     sendResponse(400, true, "Type field is not set, must be 'del' or 'dec'", [], ["audit" => $auditObj]);
-
 } elseif ($body["type"] === "dec") {
     decrementProduct();
 } elseif ($body["type"] === "del") {
