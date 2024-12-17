3526e950f8ebabbf0ca5f4a4e97227203336ee0c
redlintles <treelands54@gmail.com>
2024-12-17 18:19:24 -0300

    Função findSingle recriada

--- functions/findSingle.php
+++ functions/findSingle.php
@@ -1,57 +1,47 @@
 <?php
 
+
+function throwNotFoundError($body, $audit)
+{
+
+    sendResponse(404, true, "Object not found, If the object exists, have you set any of " . implode(",", array_keys($body)) . "?", [
+        "params" => $body
+    ], [
+        "audit" => $audit
+    ]);
+}
+
 /**
- * @param array $body the body of the requisition.
- * @param array $options an array with the keys of query(the propel query obj), keys is an array of params with key and validation pairs, throw_error is the error to be thrown if the function can't find the object.
+ * @param array $params the params the object must attend to be valid.
+ * @param array $mapping maps $params keys to database column names.
+ * @param \Propel\Runtime\ActiveQuery\ModelCriteria $queryObj The query object used to make the requisition.
+ * @param bool $throw_error Sends a not found error if the object is not found by default, if set to false, the function returns null instead
  * @return object|null returns the found object or null.
  * @throws \Exception if the object is not found and throw_error is true.
  */
-
-function findSingle(array $body, array $options): object | null
+function findSingle(array $params, array $mapping, \Propel\Runtime\ActiveQuery\ModelCriteria $queryObj, bool $throwErr, AuditObj | null $auditObj = null): object | null
 {
-    require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/dataValidation.php";
-    require_once $_SERVER["DOCUMENT_ROOT"] . "/functions/snakeToCamel.php";
-    $targetObj = null;
-
-    if (!isset($options["throw_error"])) {
-        $options["throw_error"] = true;
-    }
-
-    function throwErr($body, $audit, $options)
-    {
-
-        sendResponse(404, true, "Object not found, If the object exists, have you set any of " . implode(",", array_keys($options["keys"])) . "?", [
-            "params" => $body
-        ], [
-            $audit => $options["audit"]
-        ]);
-        throw new Exception("Object not found");
-    }
-
-    foreach ($options["keys"] as $key => $validation) {
-        if (isset($body[$key])) {
-            Validate::$validation($body[$key]);
-            $methodName = "findOneById";
-            if (!preg_match("/_id/", $key)) {
-                $methodName = "findOneBy" . ucfirst(snakeToCamel($key));
-            }
-            if (method_exists($options["query"], $methodName)) {
-                $targetObj = $options["query"]->$methodName($body[$key]);
-            } elseif ($options["throw_error"]) {
-                throwErr($body, $audit, $options);
-            } else {
-                return null;
-            }
+    $return = null;
+    foreach ($params as $key => $value) {
+        if (!key_exists($key, $mapping)) {
+            continue;
         }
-        if (isset($targetObj)) {
-            break;
+        $methodName = "filterBy" . snakeToCamel($mapping[$key], false);
+        if (method_exists($queryObj, $methodName)) {
+            $queryObj->$methodName($value);
         }
+
     }
 
-    if (!isset($targetObj) && $options["throw_error"]) {
-        throwErr($body, $audit, $options);
-    } elseif (!isset($targetObj)) {
+    $return = $queryObj->findOne();
+    if (isset($return)) {
+        return $return;
+    }
+
+    if ($throwErr) {
+        throwNotFoundError($params, $auditObj);
+    } else {
         return null;
     }
-    return $targetObj;
+
 }
