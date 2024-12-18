5a058c00327f107183209c038d9d2a2656738af0
redlintles <treelands54@gmail.com>
2024-12-17 22:22:56 -0300

    Adicionados métodos de criação, remoção e obtenção de produtos em Order

--- generated-classes/Buildings/Order.php
+++ generated-classes/Buildings/Order.php
@@ -15,5 +15,78 @@ use Buildings\Base\Order as BaseOrder;
  */
 class Order extends BaseOrder
 {
+    public function addProduct(\Buildings\Product $targetProduct, int $qtd = 1)
+    {
+        $orderRows = \Buildings\OrderProductQuery::create()->filterByIdOrder($this->getId())->find();
 
+        $alreadyInOrder = false;
+
+        foreach ($orderRows as $row) {
+            if ($targetProduct->getId() === $row->getIdProduct()) {
+                $row->setQuantity($row->getQuantity() + $qtd);
+                $alreadyInOrder = true;
+                $row->save();
+                break;
+            }
+        }
+
+        if (!$alreadyInOrder) {
+            $row = new \Buildings\OrderProduct();
+            $row->setIdOrder($this->getId());
+            $row->setIdProduct($targetProduct->getId());
+            $row->setQuantity($qtd);
+            $row->save();
+        }
+    }
+
+    public function removeProduct(\Buildings\Product $targetProduct, int $qtd = null)
+    {
+        $productRows = \Buildings\OrderProductQuery::create()->filterByIdOrder($this->getId())->find();
+
+        foreach ($productRows as $row) {
+            if ($row->getIdProduct() === $targetProduct->getId()) {
+
+                if ($qtd === null) {
+                    $qtd = $row->getQuantity();
+                }
+                $newQtd = $row->getQuantity() - $qtd > 0 ? $row->getQuantity() - $qtd : 0;
+
+
+                if ($newQtd === 0) {
+                    $row->delete();
+                } else {
+                    $row->setQuantity($newQtd);
+                    $row->save();
+                }
+                break;
+            }
+        }
+    }
+
+    public function getProducts()
+    {
+        $orderProducts = \Buildings\OrderProductQuery::create()->find();
+
+        $result = [];
+
+        $finalPrice = 0;
+
+        foreach ($orderProducts as $row) {
+            $product = \Buildings\OrderQuery::create()
+            ->useOrderProductQuery()
+                ->withColumn("OrderProduct.quantity", "quantity")
+            ->endUse()
+            ->select(array("title","description","unity_price"))
+            ->findOneById($row->getIdProduct());
+            $arr = $product;
+            $arr["final_price"] = $arr["quantity"] * $arr["unity_price"];
+            $finalPrice += $arr["final_price"];
+            array_push($result, $arr);
+        }
+        return [
+            "products" => $result,
+            "final_price" => $finalPrice
+        ];
+
+    }
 }
